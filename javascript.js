f21d2bdfff58820eb56165a3d09e89bbc695ca02
sivabharthi58 <sivabharathi1942004@gmial.com>
2024-06-20 18:08:22 +0530

    flex direction

--- javascript.js
+++ javascript.js
@@ -1,7 +1,41 @@
-var x = "sivabharathi"
+alert("hi welcome")
+/*var x = "sivabharathi"
 document.write(x)
 var y =5
 var z =100
 document.write(y+z)
 var s="welcome to hollywood"
     document .write(s.length)
+
+    var a =new Array();
+    a[0]=99
+    var b =["lion","lepored","tiger"]
+    document.write(" "+a+" ")
+    document.write(b)
+    const pen={ name :"siva",price:444,color:"red",store :"pandiyan stor"}
+        console.log("iam sivabharathi","i purshed new pen"+pen.name+"for"+pen.price+"rupees")
+        console.log(`MY pen is ${pen.name}  It's price is ${pen.price} and this color ${pen.color}`)
+        let name1="siva"
+        let name2="paramesh"
+        /*if (name1==name2){
+            console.log("yes")
+        
+        }else{
+            console.log("no")
+        }
+        name1==name2 ? console.log("yes"):console.log("no");
+       //prompt("Enter the name:")
+       random =10;
+       let guess_no=prompt("Enter the random number from 1 to 10")
+
+       while (guess_no!=random){
+        console.log("try again pzz")
+    guess_no=prompt("Enter the number")}
+    if(guess_no==random){
+        alert("congratulation you guess is correct")*/
+    }
+    const animals=["tigher","lion","elephant","deet","lepored","wolf"]
+//const animals1=['Tiger','Lion','Elephant','Deer',"wolf"]
+animals1.splice(3,2,'Cat','dog','frog');
+document.write(animals1);
+
